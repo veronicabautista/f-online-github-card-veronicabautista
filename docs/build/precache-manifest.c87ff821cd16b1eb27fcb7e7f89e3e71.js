82f77dbd97e1cb5e812bb495031b82eed97f605b
Veronica Bautista <veronicabautista87@gmail.com>
2018-11-18 20:13:07 +0100

    json homepage

---
+++ docs/build/precache-manifest.c87ff821cd16b1eb27fcb7e7f89e3e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf85df2995ef5ec7c5c3b012f050ca1",
+    "url": "./static/media/github-logo.1cf85df2.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7e37e81de0efb33a2dfb",
+    "url": "./static/js/main.7e37e81d.chunk.js"
+  },
+  {
+    "revision": "33248095f76020502333",
+    "url": "./static/js/1.33248095.chunk.js"
+  },
+  {
+    "revision": "7e37e81de0efb33a2dfb",
+    "url": "./static/css/main.c2c1690f.chunk.css"
+  },
+  {
+    "revision": "88fa628a2bdc345b16dd36e3edf33ce6",
+    "url": "./index.html"
+  }
+];
